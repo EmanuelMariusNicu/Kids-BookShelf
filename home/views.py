958101e8d29ef42909cdaeb46aca228486029609
Marius Emanuel <mariusemanuelnicusor@yahoo.com>
2023-06-25 15:36:04 +0000

    Adding html code for base, about and privacy pages

---
+++ home/views.py
@@ -0,0 +1,23 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+def index(request):
+    """
+    renders index page
+    """
+    return render(request, 'home/index.html')
+
+
+def about(request):
+    """
+    renders about page
+    """
+    return render(request, 'home/about.html')
+
+
+def privacy_policy(request):
+    """
+    renders privacy policy page
+    """
+    return render(request, 'home/privacy.html')
