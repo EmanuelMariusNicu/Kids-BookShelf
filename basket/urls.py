56fcb5c2848bc9ab15289219f21098f9f286f6ec
Marius Emanuel <mariusemanuelnicusor@yahoo.com>
2023-10-03 17:50:59 +0000

    Mistake in basket url edited

--- basket/urls.py
+++ basket/urls.py
@@ -3,7 +3,7 @@ from .import views
 
 
 urlpatterns = [
-    path(',views.basket, name='basket'),
+    path('', views.basket, name='basket'),
     path('add/<item_id>', views.add_to_basket, name='add_to_basket'),
     path('adjust/<item_id>/', views.adjust_basket, name='adjust_basket'),
     path('remove/<item_id>/', views.remove_from_basket,
